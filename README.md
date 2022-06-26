be9afc56d5e4122187e1bf4bb8aa5ccdec8738e0
Eddie Hsu <aoe102198@gmail.com>
2022-06-26 19:10:58 +0800

    Initiate the project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Crypto-Wallet-React
 Crypto-Wallet-React
+
+### Notes:
+- node: v16.14.2
---
+++ crypto-wallet/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Wallet from "./components/Wallet";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>Crypto Wallet</span>
+        <Wallet></Wallet>
+      </header>
+    </div>
+  );
+}
+
+export default App;
