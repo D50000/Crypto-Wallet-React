8c0c680703cb8d9cca35651c112c1196de1c8440
Eddie Hsu <aoe102198@gmail.com>
2022-06-26 21:49:40 +0800

    Add the searching input

--- crypto-wallet/src/components/Wallet.js
+++ crypto-wallet/src/components/Wallet.js
@@ -1,5 +1,45 @@
+import { useState, useEffect } from "react";
+
 export default function Wallet() {
-    return (
-        <span>wallet here</span>
-    )
+  const [symbolList, setSymbolList] = useState([]);
+  const [searchSymbol, setSearchSymbol] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
+  useEffect(() => {
+    fetch("https://api.binance.com/api/v3/ticker/price").then((res) =>
+      res.json().then((data) => {
+        setSymbolList(data);
+        setFilteredResults(data);
+      })
+    );
+  }, []); // Give [] for the deps to fire once.
+
+  const searchInput = (e) => {
+    // TODO: debounce
+    console.log(e.target.value);
+    setSearchSymbol(e.target.value);
+    if (searchSymbol === "") {
+      setFilteredResults(symbolList);
+    } else {
+      const result = symbolList.filter((pair) => {
+        const regex = new RegExp(`${searchSymbol}`, "i");
+        return pair.symbol.match(regex);
+      });
+      setFilteredResults(result);
+    }
+  };
+
+  return (
+    <div>
+      <span>wallet here</span>
+      <div>
+        <input onChange={searchInput} placeholder="Search..."></input>
+      </div>
+      {filteredResults.map((symbol) => (
+        <div className="container" key={symbol.symbol}>
+          <div>{symbol.symbol}</div>
+          <div>{symbol.price}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
