b900ae86ceeceb33e6615838479bbf70f4194868
Eddie Hsu <aoe102198@gmail.com>
2022-08-13 19:42:23 +0800

    Update the pair filter for usdt

--- crypto-wallet/src/components/Wallet.js
+++ crypto-wallet/src/components/Wallet.js
@@ -5,18 +5,28 @@ import ButtonBar from "./button-bar";
 export default function Wallet() {
   const [symbolList, setSymbolList] = useState([]);
   const [filteredResults, setFilteredResults] = useState([]);
+
   useEffect(() => {
     fetch("https://api.binance.com/api/v3/ticker/price").then((res) =>
       res.json().then((data) => {
         setSymbolList(data);
-        const usdRegex = new RegExp(`usdt`, "i");
-        const usdPairs = data.filter((pair) => pair.symbol.match(usdRegex));
+        const usdPairs = filterThUsdt(data);
         setFilteredResults(usdPairs);
       })
     );
-  }, []); // Give [] for the deps to fire once.
+  }, []); // Give [] for initial.
 
-  const searchInput = (e) => {
+  /**
+   * Match a single character present in the list below [\wusdt].
+   * {4} matches the previous token exactly 4 times from the end.
+   * example: *****usdt => capital ignore.
+   */
+  const filterThUsdt = (list) => {
+    const usdRegex = new RegExp(`(USDT)$`, "i");
+    return list.filter((pair) => pair.symbol.match(usdRegex));
+  };
+
+  const searchHandler = (e) => {
     // TODO: debounce
     const input = e.target.value;
     console.log(input);
@@ -40,7 +50,10 @@ export default function Wallet() {
       <span>wallet here</span>
       <ButtonBar></ButtonBar>
       <div>
-        <input onChange={(e) => searchInput(e)} placeholder="Search..."></input>
+        <input
+          onChange={(e) => searchHandler(e)}
+          placeholder="Search..."
+        ></input>
       </div>
       {filteredResults.map((symbol) => (
         <div
